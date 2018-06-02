ce055965a869f1619dccbc47ffe97df22581a5d2
big-silver <bigsilversstar@yandex.com>
2018-06-02 04:50:53 +0200

    clean the comments

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,3 @@
-/**
- * Check out https://googlechrome.github.io/sw-toolbox/ for
- * more info on how to use sw-toolbox to custom configure your service worker.
- */
-
-
 'use strict';
 importScripts('./build/sw-toolbox.js');
 
